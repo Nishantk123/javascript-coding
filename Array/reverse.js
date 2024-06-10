d8f77fdd9cc5dc0577b4dec0f439f487a7530130
Rakesh <ra02sing@gmail.com>
2024-06-10 06:54:33 +0530

    reverse

--- Array/reverse.js
+++ Array/reverse.js
@@ -1,8 +1,97 @@
-function reverse(arr){
-    let newArr = [];
-    for(let i=arr.length-1; i>=0; i--){
-       newArr += arr[i] 
-    }
-    return newArr;
- }
- console.log(reverse([5,9,7,6,3,1]));
+// function reverse(arr){
+//     let newArr = [];
+//     for(let i=arr.length-1; i>=0; i--){
+//        newArr += arr[i] 
+//     }
+//     return newArr;
+//  }
+//  console.log(reverse([5,9,7,6,3,1]));
+
+        // find max value
+    //  function Max(arr){
+    //     let largest = 0;
+    //     for(let i=0; i<=arr.length; i++){
+    //         if(arr[i] > largest){
+    //             largest = arr[i]
+    //         }
+    //     }
+    //     return largest;
+    //  }
+    //  console.log(Max([5,9,7,6,3,1]))
+
+          //find minimum value
+        //   function Min(arr){
+        //     let smallest = arr[0];
+        //     for(let i=0; i<=arr.length; i++){
+        //         if(arr[i] < smallest){
+        //             smallest = arr[i]
+        //         }
+        //     }
+        //     return smallest;
+        //   }
+    //   console.log(Min([5,9,7,6,3,1,8]))1q`
+
+          // find sum of numbers in array
+        //   function Sum(arr){
+            // let sum = 0;
+            // for(let i=0; i< arr.length; i++){
+            //     sum += arr[i]
+            // }
+            // return sum;
+        //     let sum = arr.reduce((acc,curr)=>{
+        //           acc += curr
+        //     },0)
+        //     return sum;
+        //   }
+        //   console.log(Sum([5,9,7,6,3,1,8]))
+
+              // Average of element
+            //   function Average(arr){
+            //     let sum= 0;
+            //     for(let i=0; i<arr.length; i++){
+            //         sum += arr[i]
+            //     }
+            //     return sum/arr.length
+            //   }
+            //   console.log(Average([5,8,3,7,6]))
+
+            // function removeNum(arr,num){
+            //     let newArr = [];
+            //     for(let i=0; i<arr.length; i++){
+            //      if(num !== arr[i]){
+            //         newArr.push(arr[i])
+            //      }
+            //     }
+            //     return newArr;
+            // }
+            // console.log(removeNum([8,2,6,4,2,3],2))
+
+            // function secLargest(arr){
+            //     let largest = 0;
+            //     let sec_large = 0;
+            //     for(let i=0; i<arr.length; i++){
+            //         if(arr[i] > largest){
+            //            sec_large = largest  
+            //            largest = arr[i]
+            //         }else if(arr[i] > sec_large && arr[i] !== largest){
+            //             sec_large = arr[i]
+            //         }
+            //     }
+            //     return sec_large;
+            // }
+            // console.log(secLargest([8,2,6,4,2,3]))
+
+            function secSmallest(arr){
+                let smallest = Infinity;
+                let sec_small = Infinity;
+                for(let i=0; i<arr.length; i++){
+                    if(arr[i] < smallest){
+                        sec_small = smallest
+                        smallest = arr[i]
+                    }else if(arr[i] < sec_small && arr[i] !== smallest){
+                        sec_small = arr[i]
+                    }
+                }
+                return sec_small;
+            }
+            console.log(secSmallest([8,2,6,4,2,3]))
